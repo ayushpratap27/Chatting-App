2f49838d450d3e53f2620831c5db02400574b97d
Ayush Pratap <ayushpratap2127@gmail.com>
2025-01-11 09:42:15 +0530

    Add profile update functionality, new avatar component

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -96,3 +96,38 @@ export const getUserInfo = async (req, res, next) => {
         return res.status(500).send("Internal server error");
     }
 };
+
+export const updateProfile = async (req, res, next) => {
+    try {
+        const { userId } = req;
+        const { firstName, lastName, color } = req.body;
+        if(!firstName || !lastName) {
+            return res.status(400).send("First name, last name, and color are required");
+        }
+
+        const userData = await User.findByIdAndUpdate(
+            userId,
+            {
+                firstName,
+                lastName,
+                color,
+                profileSetup: true,
+            },
+            { new: true, runValidators: true }
+        ); 
+
+        return res.status(200).json({
+            id: userData.id,
+            email: userData.email,
+            profileSetup: userData.profileSetup,
+            firstName: userData.firstName,
+            lastName: userData.lastName,
+            image: userData.image,
+            color: userData.color,
+
+        });
+    } catch (error) {
+        console.log({error});
+        return res.status(500).send("Internal server error");
+    }
+};
