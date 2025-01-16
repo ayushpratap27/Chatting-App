51ed27c9c4ee173c99bf1697daf40f3bda84ab89
Ayush Pratap <ayushpratap2127@gmail.com>
2025-01-16 14:30:57 +0530

    Add socket.io integration for real-time communication

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,12 +1,12 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { Toaster } from 'sonner'
+import { SocketProvider } from './context/socketContext'
 
 createRoot(document.getElementById('root')).render(
-  <>
+  <SocketProvider>
     <App />
     <Toaster closeButton/>
-  </>
+  </SocketProvider>
 )
