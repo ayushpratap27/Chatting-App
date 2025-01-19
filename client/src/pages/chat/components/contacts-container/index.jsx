d094f75bd8bcac0bbc8ab98aba0a99407f5a19ee
Ayush Pratap <ayushpratap2127@gmail.com>
2025-01-19 12:20:25 +0530

    Add channel modal UI

--- client/src/pages/chat/components/contacts-container/index.jsx
+++ client/src/pages/chat/components/contacts-container/index.jsx
@@ -5,6 +5,7 @@ import { apiClient } from "@/lib/api-client";
 import { GET_DM_CONTACTS_ROUTES } from "@/utils/constants";
 import { useAppStore } from "@/store";
 import ContactList from "@/components/ui/contact-list";
+import CreateChannel from "./components/create-channel";
 
 function ContactsContainer() {
 
@@ -40,6 +41,7 @@ function ContactsContainer() {
         <div className="my-5">
             <div className="flex items-center justify-between pr-10">
                 <Title text="Channels" />
+                <CreateChannel/>
             </div>
         </div>
         <ProfileInfo/>
