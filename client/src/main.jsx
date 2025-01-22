ca76626623c921ea7e76fa2504ebb2b8f587c2f8
Ayush Pratap <ayushpratap2127@gmail.com>
2025-01-22 23:16:25 +0530

    Fix import case for SocketProvider in main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,7 +2,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { Toaster } from 'sonner'
-import { SocketProvider } from './context/socketContext'
+import { SocketProvider } from './context/SocketContext'
 
 createRoot(document.getElementById('root')).render(
   <SocketProvider>
